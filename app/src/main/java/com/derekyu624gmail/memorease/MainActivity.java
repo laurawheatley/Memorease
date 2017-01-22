4e0d1791344938d697dd2375df9f1eae4b02a561
Derek Yu <derekyu624@hotmail.com>
2017-01-21 23:07:11 -0500

    Added speech-to-text recognition in MainActivity.java

--- app/src/main/java/com/derekyu624gmail/memorease/MainActivity.java
+++ app/src/main/java/com/derekyu624gmail/memorease/MainActivity.java
@@ -1,6 +1,9 @@
 package com.derekyu624gmail.memorease;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -12,14 +15,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private TextView resultTEXT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resultTEXT = (TextView) findViewById(R.id.TVresult);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -35,11 +46,44 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    public void onButtonClick(View v){
+        if (v.getId() == R.id.imageButton) {
+            promptSpeechInput();
+        }
+    }
+
+    public void promptSpeechInput(){
+        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say something");
+
+        try{
+            startActivityForResult(i, 100);
+        }
+        catch(ActivityNotFoundException a){
+            Toast.makeText(MainActivity.this, "Sorry. Your device does not support speech language.",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent i){
+        super.onActivityResult(requestCode, resultCode, i);
+
+        switch(requestCode){
+            case 100:
+                if (resultCode == RESULT_OK && i != null){
+                    ArrayList<String> result = i.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    resultTEXT.setText(result.get(0));
+                }
+                break;
+        }
     }
 
     @Override
